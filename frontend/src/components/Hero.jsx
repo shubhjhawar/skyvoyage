c75aa9d077e86fcad1e4e74227f2ab383292412d
Shubh <shubhjhawar78@gmail.com>
2023-08-02 22:15:59 -0400

    half banner done motion div

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,27 +1,46 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import {banner, deal} from "../assets";
 import { styles } from '../styles';
+import { fadeIn, slideIn } from '../utils/motion';
+
 
 const Hero = () => {
   return (
-    <div>
-        <div className="">
-            <img src={banner} alt="banner" className="w-full -mt-[12px]"/>
+    <div className="">
+        <div className="relative">
+            <img src={banner} alt="banner" className="w-full -mt-[12px] z-[0]"/>
+            <motion.p
+              variants={slideIn('left', 'spring', 0.5, 2)}
+              initial="hidden" 
+              animate="show" 
+              className="absolute top-0 text-[200px] font-semibold uppercase z-10 text-blue-200 px-2 text-shadow-md "
+            >
+              skyvoyage
+            </motion.p>
+            <motion.p
+              variants={slideIn('right', 'tweep', 0.25, 2)}
+              initial="hidden" 
+              animate="show" 
+              className="absolute top-[220px] text-[30px] mt-[20px] font-semibold  z-10 text-blue-700 px-2 ml-2 italic"
+            >
+              Your Journey to the Skies...
+            </motion.p>
         </div>
 
         <div className="flex flex-row justify-center items-center -mt-[100px]">
           <a className={`${styles.bannerIcon}`} href="#deal">
             <img src={deal} alt="background" className='absolute w-[250px] h-[250px] rounded-xl object-contain z-[0] '/>
-            <h2 className='relative text-[50px] font-semibold ml-2 text-blue-600'>Deal</h2>
+            <h2 className='relative text-[50px] font-semibold ml-2 text-blue-600 '>Deal</h2>
             <p className='relative m-2 font-semibold  text-red-600 text-[19px]'>20% Off Flights to Europe:</p>
             <p className='relative m-2 text-[17px] mt-3 font-semibold leading-[25px]'>Book your flights to Europe within the next 72 hours and get an exclusive 20% discount on your ticket price</p>
           </a>
-          <a className={`${styles.bannerIcon} bg-black`}>
+          <a className={`${styles.bannerIcon} bg-black z-10`}>
             
           </a>
 
-          <a className={`${styles.bannerIcon} bg-blue-600`}>
+          <a className={`${styles.bannerIcon} bg-blue-600 z-10`}>
             <h2 className='relative text-[50px] font-semibold ml-2 text-white'>Fly Pass</h2>
             <p className='relative m-2 font-semibold mt-3 text-[#263547] text-[25px] flex flex-col justify-center items-center'>Unlock the Skies: Fly Pass – Your Ticket to Freedom!</p>
           </a>
