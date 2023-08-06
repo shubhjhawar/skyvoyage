bc6babb68ed5d00d03d3fd15f2ecde3c8ba167f4
Shubh <shubhjhawar78@gmail.com>
2023-08-05 21:35:41 -0400

    changed allowd hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)&3e8&!*1l!3g*59h56lh_-8czf@65_h+so=5_udye**uehaxi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 
 # Application definition
